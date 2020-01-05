5a42bccb620c01f979509a3f2da9a8f01e9ac679
Dimitrios Strantsalis <dstrants@gmail.com>
2020-01-05 23:22:21 +0200

    protects all views with login

--- tests/views.py
+++ tests/views.py
@@ -1,19 +1,22 @@
-import os
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from .helpers import locate_docxs, import_docxs
 from .models import Attempt, Question, Aswer, Choice
+import os
 
 
 def index(request):
     return render(request, "tests/index.html")
 
 
+@login_required
 def start_import(request):
     files = locate_docxs()
     return render(request, "tests/start_import.html", {'files': files})
 
 
+@login_required
 def import_tests(request):
     files = locate_docxs()
     try:
@@ -23,6 +26,7 @@ def import_tests(request):
     return redirect('test:index')
 
 
+@login_required
 def start_test(request):
     qset = Question.objects.none()
     questions = Question.objects.all()
@@ -37,6 +41,8 @@ def start_test(request):
         Aswer.objects.create(question=q, attempt=attempt)
     return render(request, "tests/test.html", {'aswers': attempt.aswer_set.all(), 'attempt': attempt})
 
+
+@login_required
 def submit_answer(request):
     ch_id = request.GET.get('ch_id')
     choice = get_object_or_404(Choice, id=int(ch_id))
@@ -47,6 +53,7 @@ def submit_answer(request):
     return JsonResponse({'status': 'done'})
 
 
+@login_required
 def finish_test(request):
     at_id = request.GET.get('at_id')
     attempt = get_object_or_404(Attempt, id=at_id)
@@ -58,11 +65,13 @@ def finish_test(request):
     return render(request, 'tests/results.html', {'attempt': attempt})
 
 
+@login_required
 def attempts_list(request):
     attempts = request.user.attempt_set.all()
     return render(request, "tests/attemps.html", {'attempts': attempts})
 
 
+@login_required
 def attempt_detail(request, id):
     attempt = get_object_or_404(Attempt, id=id)
     return render(request, "tests/results.html", {'attempt': attempt})
